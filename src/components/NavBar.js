da4694262879775cc4277a7dd727fa84f0a7b852
Tim Schulz <tim@getbej.com>
2022-05-29 10:47:06 +0000

    Edit Navbar styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,22 +5,22 @@ import styles from '../styles/NavBar.module.css'
 
 const NavBar = () => {
   return (
-    <Navbar expand="md" fixed="top" className={styles.NavBar}>
+    <Navbar expand="md" fixed="top" variant="dark">
       <Container >
-        <Navbar.Brand className={styles.Brand}>
+        <Navbar.Brand className={styles.NavColor}>
           <h1 >8GAG</h1>
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Toggle aria-controls="basic-navbar-nav"/>
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ml-auto text-left">
+          <Nav className="ml-md-auto text-center">
             <Nav.Link>
-              Home
+              <span>Home</span>
             </Nav.Link>
             <Nav.Link>
-              Sign in
+              <span>Sign In</span>
             </Nav.Link>
             <Nav.Link>
-              Sign up
+              <span>Sign Up</span>
             </Nav.Link>
           </Nav>
         </Navbar.Collapse>
