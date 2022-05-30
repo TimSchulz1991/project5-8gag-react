a2f1e10f1d62b4a8d4dbd6dcb0ecc139572cee1a
Tim Schulz <tim@getbej.com>
2022-05-30 14:37:37 +0000

    Implement Signin form incl. logic and styling

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import Container from "react-bootstrap/Container";
 import { Route, Switch } from "react-router-dom";
 import "./api/axiosDefaults";
 import SignUpForm from './pages/auth/SignUpForm'
+import SignInForm from "./pages/auth/SignInForm";
 
 function App() {
+
     return (
         <div className={styles.App}>
             <NavBar />
@@ -15,7 +17,7 @@ function App() {
                     <Route
                         exact
                         path="/signin"
-                        render={() => <h1>Sign in</h1>}
+                        render={() => <SignInForm />}
                     />
                     <Route
                         exact
--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -1,39 +1,47 @@
 import React, { useState } from "react";
+
+import Form from "react-bootstrap/Form";
+import Alert from "react-bootstrap/Alert";
+import Button from "react-bootstrap/Button";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Image from "react-bootstrap/Image";
+import Container from "react-bootstrap/Container";
+
 import { Link, useHistory } from "react-router-dom";
 import axios from "axios";
-import { Alert } from "react-bootstrap";
 
 import styles from "../../styles/SignInUpForm.module.css";
 import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css";
+import meme from "../../assets/burningHouseMeme.jpeg";
 
-import { Form, Button, Image, Col, Row, Container } from "react-bootstrap";
-import mainMeme from "../../assets/mainMeme.jpeg";
-
-const SignUpForm = () => {
-
-    const [signUpData, setSignUpData] = useState({
+function SignInForm() {
+    const [signInData, setSignInData] = useState({
         username: "",
-        password1: "",
-        password2: "",
+        password: "",
     });
-    const { username, password1, password2 } = signUpData;
+    const [errors, setErrors] = useState({});
+    const { username, password } = signInData;
     const history = useHistory();
 
-    const [errors, setErrors] = useState({});
-
     const handleChange = (e) => {
-        setSignUpData({
-            ...signUpData,
-            [e.target.name]: encodeURI.target.value,
+        setSignInData({
+            ...signInData,
+            [e.target.name]: e.target.value,
         });
     };
 
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            await axios.post("/dj-rest-auth/registration/", signUpData);
-            history.push("/signin");
+            const { data } = await axios.post(
+                "/dj-rest-auth/login/",
+                signInData
+            );
+            // setCurrentUser(data.user);
+            // setTokenTimestamp(data);
+            history.push("/")
         } catch (err) {
             setErrors(err.response?.data);
         }
@@ -41,10 +49,9 @@ const SignUpForm = () => {
 
     return (
         <Row className={styles.Row}>
-            <Col className="my-auto py-2 p-md-2" md={6}>
+            <Col className="my-auto p-0 p-md-2" md={6}>
                 <Container className={`${appStyles.Content} p-4 `}>
-                    <h1 className={styles.Header}>sign up</h1>
-
+                    <h1 className={styles.Header}>sign in</h1>
                     <Form onSubmit={handleSubmit}>
                         <Form.Group controlId="username">
                             <Form.Label className="d-none">Username</Form.Label>
@@ -63,37 +70,18 @@ const SignUpForm = () => {
                             </Alert>
                         ))}
 
-                        <Form.Group controlId="password1">
+                        <Form.Group controlId="password">
                             <Form.Label className="d-none">Password</Form.Label>
                             <Form.Control
                                 classname={styles.Input}
                                 type="password"
                                 placeholder="Password"
-                                name="password1"
-                                value={password1}
+                                name="password"
+                                value={password}
                                 onChange={handleChange}
                             />
                         </Form.Group>
-                        {errors.password1?.map((message, idx) => (
-                            <Alert variant="warning" key={idx}>
-                                {message}
-                            </Alert>
-                        ))}
-
-                        <Form.Group controlId="password2">
-                            <Form.Label className="d-none">
-                                Confirm password
-                            </Form.Label>
-                            <Form.Control
-                                classname={styles.Input}
-                                type="password"
-                                placeholder="Confirm password"
-                                name="password2"
-                                value={password2}
-                                onChange={handleChange}
-                            />
-                        </Form.Group>
-                        {errors.password2?.map((message, idx) => (
+                        {errors.password?.map((message, idx) => (
                             <Alert variant="warning" key={idx}>
                                 {message}
                             </Alert>
@@ -103,7 +91,7 @@ const SignUpForm = () => {
                             className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright}`}
                             type="submit"
                         >
-                            Sign up
+                            Sign in
                         </Button>
                         {errors.non_field_errors?.map((message, idx) => (
                             <Alert key={idx} variant="warning" className="mt-3">
@@ -113,19 +101,19 @@ const SignUpForm = () => {
                     </Form>
                 </Container>
                 <Container className={`mt-3 ${appStyles.Content}`}>
-                    <Link className={styles.Link} to="/signin">
-                        Already have an account? <span>Sign in</span>
+                    <Link className={styles.Link} to="/signup">
+                        Don't have an account? <span>Sign up now!</span>
                     </Link>
                 </Container>
             </Col>
             <Col
                 md={6}
-                className={`my-auto d-none d-md-block p-2 ${styles.SignUpCol}`}
+                className={`my-auto d-none d-md-block p-2 ${styles.SignInCol}`}
             >
-                <Image className={`${appStyles.FillerImage}`} src={mainMeme} />
+                <Image className={`${appStyles.FillerImage}`} src={meme} />
             </Col>
         </Row>
     );
-};
+}
 
-export default SignUpForm;
+export default SignInForm;
