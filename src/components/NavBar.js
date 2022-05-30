c25741a4c3f6686fccd7d1b244166a00d088d1e1
Tim Schulz <tim@getbej.com>
2022-05-30 12:09:28 +0000

    Setting up routing properly and fix styling issues in Navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,32 +1,52 @@
 import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
-import styles from '../styles/NavBar.module.css'
-
+import styles from "../styles/NavBar.module.css";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
-  return (
-    <Navbar expand="md" fixed="top" variant="dark" className={styles.NavBar}>
-      <Container >
-        <Navbar.Brand>
-          <h1 className="mb-0">8GAG</h1>
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ml-md-auto text-center">
-            <Nav.Link>
-              Home
-            </Nav.Link>
-            <Nav.Link>
-              Sign In
-            </Nav.Link>
-            <Nav.Link>
-              Sign Up
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  );
+    return (
+        <Navbar
+            expand="md"
+            fixed="top"
+            variant="dark"
+            className={styles.NavBar}
+        >
+            <Container>
+                <NavLink to="/">
+                    <Navbar.Brand>
+                        <h1 className="mb-0">8GAG</h1>
+                    </Navbar.Brand>
+                </NavLink>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ml-md-auto text-center">
+                        <NavLink
+                            exact
+                            className="ml-3"
+                            activeClassName={styles.Active}
+                            to="/"
+                        >
+                            Home
+                        </NavLink>
+                        <NavLink
+                            className="ml-3"
+                            activeClassName={styles.Active}
+                            to="/signin"
+                        >
+                            Sign In
+                        </NavLink>
+                        <NavLink
+                            className="ml-3"
+                            activeClassName={styles.Active}
+                            to="/signup"
+                        >
+                            Sign Up
+                        </NavLink>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
 };
 
 export default NavBar;
