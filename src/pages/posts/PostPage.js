77128a74128bd68f4250b0888ade12daea64d84e
Tim Schulz <tim@getbej.com>
2022-06-06 12:17:57 +0000

    Add the Comment Create form and style it

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -11,32 +11,50 @@ import Container from "react-bootstrap/Container";
 import appStyles from "../../App.module.css";
 import Post from "./Post";
 
+import CommentCreateForm from "../comments/CommentCreateForm";
+import { useCurrentUser } from "../../context/CurrentUserContext";
+
 function PostPage() {
     const { id } = useParams();
     const [post, setPost] = useState({ results: [] });
 
+    const currentUser = useCurrentUser();
+    const profile_image = currentUser?.profile_image;
+    const [comments, setComments] = useState({ results: [] });
+
     useEffect(() => {
-      const handleMount = async () => {
-          try {
-              const [{ data: post }] = await Promise.all([
-                  axiosReq.get(`/posts/${id}`),
-                  // axiosReq.get(`/comments/?post=${id}`),
-              ]);
-              setPost({ results: [post] });
-              console.log(post);
-              // setComments(comments);
-          } catch (err) {
-              console.log(err);
-          }
-      };
-      handleMount();
-  }, [id]);
+        const handleMount = async () => {
+            try {
+                const [{ data: post }, { data: comments }] = await Promise.all([
+                    axiosReq.get(`/posts/${id}`),
+                    axiosReq.get(`/comments/?post=${id}`),
+                ]);
+                setPost({ results: [post] });
+                setComments(comments);
+            } catch (err) {
+                console.log(err);
+            }
+        };
+        handleMount();
+    }, [id]);
 
     return (
         <Row className="h-100">
             <Col className="py-2 p-0 p-lg-2" lg={{ span: 8, offset: 2 }}>
                 <Post {...post.results[0]} setPosts={setPost} postPage />
-                <Container className={appStyles.Content}>Comments</Container>
+                <Container className={appStyles.Content}>
+                    {currentUser ? (
+                        <CommentCreateForm
+                            profile_id={currentUser.profile_id}
+                            profileImage={profile_image}
+                            post={id}
+                            setPost={setPost}
+                            setComments={setComments}
+                        />
+                    ) : comments.results.length ? (
+                        "Comments"
+                    ) : null}
+                </Container>
             </Col>
         </Row>
     );
