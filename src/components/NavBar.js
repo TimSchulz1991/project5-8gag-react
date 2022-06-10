1f2b0a08521834a50be101b1d31f2b85bfa1d1fe
Ali Farooq <alifarooq96@gmail.com>
2022-06-11 03:42:09 +0500

    Update frontend to use bearer token

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,123 +6,114 @@ import styles from "../styles/NavBar.module.css";
 import btnStyles from "../styles/Button.module.css";
 import { NavLink } from "react-router-dom";
 import {
-    useCurrentUser,
-    useSetCurrentUser,
+  useCurrentUser,
+  useSetCurrentUser,
 } from "../context/CurrentUserContext";
 import Avatar from "./Avatar";
 import axios from "axios";
 import useClickOutsideToggle from "../hooks/UseClickOutsideToggle";
-import { removeTokenTimestamp } from "../utils/utils";
+import { clearTokens, removeTokenTimestamp } from "../utils/utils";
 
 const NavBar = () => {
-    const currentUser = useCurrentUser();
-    const setCurrentUser = useSetCurrentUser();
+  const currentUser = useCurrentUser();
+  const setCurrentUser = useSetCurrentUser();
 
-    const { expanded, setExpanded, ref } = useClickOutsideToggle();
+  const { expanded, setExpanded, ref } = useClickOutsideToggle();
 
-    const handleSignOut = async () => {
-        try {
-            await axios.post("dj-rest-auth/logout/");
-            setCurrentUser(null);
-            removeTokenTimestamp();
-        } catch (err) {
-            // console.log(err);
-        }
-    };
+  const handleSignOut = async () => {
+    try {
+      setCurrentUser(null);
+      clearTokens();
+    } catch (err) {
+      // console.log(err);
+    }
+  };
 
-    const addPostIcon = (
-        <NavLink
-            to="/posts/create"
-            className={`${btnStyles.Button} ${btnStyles.Bright}`}
-        >
-            Create new post
+  const addPostIcon = (
+    <NavLink
+      to="/posts/create"
+      className={`${btnStyles.Button} ${btnStyles.Bright}`}
+    >
+      Create new post
+    </NavLink>
+  );
+
+  const loggedInIcons = (
+    <>
+      <NavLink
+        className="ml-3 my-auto p-1"
+        activeClassName={styles.Active}
+        to="/liked"
+      >
+        Liked
+      </NavLink>
+      <NavLink className="ml-3 my-auto p-1" to="/" onClick={handleSignOut}>
+        Sign out
+      </NavLink>
+      <NavLink
+        className="ml-3 my-auto p-1"
+        activeClassName={styles.Active}
+        to={`/profiles/${currentUser?.profile_id}`}
+      >
+        <Avatar src={currentUser?.profile_image} text="Profile" height={40} />
+      </NavLink>
+    </>
+  );
+  const loggedOutIcons = (
+    <>
+      <NavLink
+        className="ml-3 my-auto p-1"
+        activeClassName={styles.Active}
+        to="/signin"
+      >
+        Sign In
+      </NavLink>
+      <NavLink
+        className="ml-3 my-auto p-1"
+        activeClassName={styles.Active}
+        to="/signup"
+      >
+        Sign Up
+      </NavLink>
+    </>
+  );
+
+  return (
+    <Navbar
+      expanded={expanded}
+      expand="md"
+      fixed="top"
+      variant="dark"
+      className={styles.NavBar}
+    >
+      <Container>
+        <NavLink to="/" onClick={() => (window.location.href = "/")}>
+          <Navbar.Brand>
+            <h1 className="mb-0">8GAG</h1>
+          </Navbar.Brand>
         </NavLink>
-    );
-
-    const loggedInIcons = (
-        <>
+        {currentUser && addPostIcon}
+        <Navbar.Toggle
+          ref={ref}
+          onClick={() => setExpanded(!expanded)}
+          aria-controls="basic-navbar-nav"
+        />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-md-auto text-center">
             <NavLink
-                className="ml-3 my-auto p-1"
-                activeClassName={styles.Active}
-                to="/liked"
+              exact
+              className="ml-3 my-auto p-1"
+              activeClassName={styles.Active}
+              to="/"
             >
-                Liked
+              Home
             </NavLink>
-            <NavLink
-                className="ml-3 my-auto p-1"
-                to="/"
-                onClick={handleSignOut}
-            >
-                Sign out
-            </NavLink>
-            <NavLink
-                className="ml-3 my-auto p-1"
-                activeClassName={styles.Active}
-                to={`/profiles/${currentUser?.profile_id}`}
-            >
-                <Avatar
-                    src={currentUser?.profile_image}
-                    text="Profile"
-                    height={40}
-                />
-            </NavLink>
-        </>
-    );
-    const loggedOutIcons = (
-        <>
-            <NavLink
-                className="ml-3 my-auto p-1"
-                activeClassName={styles.Active}
-                to="/signin"
-            >
-                Sign In
-            </NavLink>
-            <NavLink
-                className="ml-3 my-auto p-1"
-                activeClassName={styles.Active}
-                to="/signup"
-            >
-                Sign Up
-            </NavLink>
-        </>
-    );
-
-    return (
-        <Navbar
-            expanded={expanded}
-            expand="md"
-            fixed="top"
-            variant="dark"
-            className={styles.NavBar}
-        >
-            <Container>
-                <NavLink to="/" onClick={() => window.location.href = "/"}>
-                    <Navbar.Brand>
-                        <h1 className="mb-0">8GAG</h1>
-                    </Navbar.Brand>
-                </NavLink>
-                {currentUser && addPostIcon}
-                <Navbar.Toggle
-                    ref={ref}
-                    onClick={() => setExpanded(!expanded)}
-                    aria-controls="basic-navbar-nav"
-                />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="ml-md-auto text-center">
-                        <NavLink
-                            exact
-                            className="ml-3 my-auto p-1"
-                            activeClassName={styles.Active}
-                            to="/"
-                        >
-                            Home
-                        </NavLink>
-                        {currentUser ? loggedInIcons : loggedOutIcons}
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
-    );
+            {currentUser ? loggedInIcons : loggedOutIcons}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default NavBar;
--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -17,110 +17,111 @@ import appStyles from "../../App.module.css";
 import meme from "../../assets/burningHouseMeme.jpeg";
 import { useSetCurrentUser } from "../../context/CurrentUserContext";
 import { useRedirect } from "../../hooks/UseRedirect";
-import { setTokenTimestamp } from "../../utils/utils";
+import { setTokens } from "../../utils/utils";
 
 function SignInForm() {
+  const setCurrentUser = useSetCurrentUser();
+  useRedirect("loggedIn");
 
-    const setCurrentUser = useSetCurrentUser();
-    useRedirect("loggedIn")
+  const [signInData, setSignInData] = useState({
+    username: "",
+    password: "",
+  });
+  const [errors, setErrors] = useState({});
+  const { username, password } = signInData;
+  const history = useHistory();
 
-    const [signInData, setSignInData] = useState({
-        username: "",
-        password: "",
+  const handleChange = (e) => {
+    setSignInData({
+      ...signInData,
+      [e.target.name]: e.target.value,
     });
-    const [errors, setErrors] = useState({});
-    const { username, password } = signInData;
-    const history = useHistory();
+  };
 
-    const handleChange = (e) => {
-        setSignInData({
-            ...signInData,
-            [e.target.name]: e.target.value,
-        });
-    };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios.post("/dj-rest-auth/", signInData);
+      const { profile_id, username, profile_image } = data;
+      setCurrentUser({
+        profile_id,
+        username,
+        profile_image,
+      });
+      setTokens(data);
+      history.goBack();
+    } catch (err) {
+      setErrors(err.response?.data);
+    }
+  };
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        try {
-            const { data } = await axios.post(
-                "/dj-rest-auth/login/",
-                signInData
-            );
-            setCurrentUser(data.user);
-            setTokenTimestamp(data);
-            history.goBack();
-        } catch (err) {
-            setErrors(err.response?.data);
-        }
-    };
+  return (
+    <Row className={styles.Row}>
+      <Col className="my-auto py-2 p-md-2" md={6}>
+        <Container className={`${appStyles.Content} p-4 `}>
+          <h1 className={styles.Header}>sign in</h1>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group controlId="username">
+              <Form.Label className="d-none">Username</Form.Label>
+              <Form.Control
+                className={styles.Input}
+                type="text"
+                placeholder="Username"
+                name="username"
+                value={username}
+                onChange={handleChange}
+              />
+            </Form.Group>
+            {errors.username?.map((message, idx) => (
+              <Alert variant="warning" key={idx}>
+                {message}
+              </Alert>
+            ))}
 
-    return (
-        <Row className={styles.Row}>
-            <Col className="my-auto py-2 p-md-2" md={6}>
-                <Container className={`${appStyles.Content} p-4 `}>
-                    <h1 className={styles.Header}>sign in</h1>
-                    <Form onSubmit={handleSubmit}>
-                        <Form.Group controlId="username">
-                            <Form.Label className="d-none">Username</Form.Label>
-                            <Form.Control
-                                className={styles.Input}
-                                type="text"
-                                placeholder="Username"
-                                name="username"
-                                value={username}
-                                onChange={handleChange}
-                            />
-                        </Form.Group>
-                        {errors.username?.map((message, idx) => (
-                            <Alert variant="warning" key={idx}>
-                                {message}
-                            </Alert>
-                        ))}
+            <Form.Group controlId="password">
+              <Form.Label className="d-none">Password</Form.Label>
+              <Form.Control
+                className={styles.Input}
+                type="password"
+                placeholder="Password"
+                name="password"
+                value={password}
+                onChange={handleChange}
+              />
+            </Form.Group>
+            {errors.password?.map((message, idx) => (
+              <Alert variant="warning" key={idx}>
+                {message}
+              </Alert>
+            ))}
 
-                        <Form.Group controlId="password">
-                            <Form.Label className="d-none">Password</Form.Label>
-                            <Form.Control
-                                className={styles.Input}
-                                type="password"
-                                placeholder="Password"
-                                name="password"
-                                value={password}
-                                onChange={handleChange}
-                            />
-                        </Form.Group>
-                        {errors.password?.map((message, idx) => (
-                            <Alert variant="warning" key={idx}>
-                                {message}
-                            </Alert>
-                        ))}
-
-                        <Button
-                            className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright}`}
-                            type="submit"
-                        >
-                            Sign in
-                        </Button>
-                        {errors.non_field_errors?.map((message, idx) => (
-                            <Alert key={idx} variant="warning" className="mt-3">
-                                {message}
-                            </Alert>
-                        ))}
-                    </Form>
-                </Container>
-                <Container className={`mt-3 ${appStyles.Content}`}>
-                    <Link className={styles.Link} to="/signup">
-                        Don't have an account? <span>Sign up now!</span>
-                    </Link>
-                </Container>
-            </Col>
-            <Col
-                md={6}
-                className={`my-auto d-none d-md-block p-2 ${styles.SignInCol}`}
+            <Button
+              className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright}`}
+              type="submit"
             >
-                <Image className={`${appStyles.FillerImage}`} src={meme} />
-            </Col>
-        </Row>
-    );
+              Sign in
+            </Button>
+            {errors.non_field_errors?.map((message, idx) => (
+              <Alert key={idx} variant="warning" className="mt-3">
+                {message}
+              </Alert>
+            ))}
+          </Form>
+        </Container>
+        <Container className={`mt-3 ${appStyles.Content}`}>
+          <Link className={styles.Link} to="/signup">
+            Don't have an account? <span>Sign up now!</span>
+          </Link>
+        </Container>
+      </Col>
+      <Col
+        md={6}
+        className={`my-auto d-none d-md-block p-2 ${styles.SignInCol}`}
+      >
+        <Image className={`${appStyles.FillerImage}`} src={meme} />
+      </Col>
+    </Row>
+  );
 }
 
 export default SignInForm;
